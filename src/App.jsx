50b703f746a84b338255f0408289b5809188cdd9
Cornerstone <fortunecornerstone@gmail.com>
2022-11-23 14:15:41 +0100

    feat: Add delete function

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,10 @@ db.version(1).stores({
 const { todos } = db;
 
 const App = () => {
-  const allItems = useLiveQuery(() => todos.toArray(), []);
-
   const [displayArrow, setDisplayArrow] = useState(false);
-  const [done, setDone] = useState(false);
   const [taskInput, setTaskInput] = useState("");
+  const [container, setContainer] = useState(false);
+  const allItems = useLiveQuery(() => todos.toArray(), []);
 
   const scrollUp = () => {
     window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
@@ -31,26 +30,36 @@ const App = () => {
     }
   };
 
+  
+  const addTask = async (e) => {
+    e.preventDefault();
+    if (taskInput) {
+      await todos.add({
+        task: taskInput,
+        completed: false,
+      });
+      setTaskInput("");
+      setContainer(true);
+    } else {
+      alert("Please add a task.");
+    }
+  };
+  
+  const deleteTask = async (id) => await todos.delete(id);
+  
+  const toggleStatus = async (id, e) => {
+    await todos.update(id, { completed: !!e.target.checked });
+  };
+  
   useEffect(() => {
     window.addEventListener("scroll", listenToScroll);
     return () => window.removeEventListener("scroll", listenToScroll);
   }, []);
 
-  const addTask = async (e) => {
-    e.preventDefault();
-
-    await todos.add({
-      task: taskInput,
-      completed: false,
-    });
-
-    setTaskInput("");
-  };
-
   return (
     <div className="w-full min-h-screen flex justify-center items-start py-[30px]">
       <div className="w-[90%] lg:w-[50%] flex flex-col gap-[40px] items-center">
-        <h1 className="text-teal-700 font-bold text-2xl">Todo App</h1>
+        <h1 className="text-slate-700 font-bold text-2xl">Todo App</h1>
         <form
           className="w-full flex flex-col gap-[6px] justify-end items-end"
           onSubmit={addTask}
@@ -60,34 +69,42 @@ const App = () => {
             name=""
             id=""
             placeholder="What are you doing today?"
-            className="w-full outline-none border-b-2 border-teal-700 pb-[8px] pl-[5px]"
+            className="w-full outline-none border-b-2 border-slate-700 pb-[8px] pl-[5px]"
             value={taskInput}
             onChange={(e) => setTaskInput(e.target.value)}
           />
-          <button className="text-white font-semibold text-base bg-teal-700 px-[20px] py-[5px] uppercase shadow-md shadow-slate-500">
+          <button className="text-white font-semibold text-base bg-slate-700 px-[20px] py-[5px] uppercase shadow-md shadow-slate-500">
             Add
           </button>
         </form>
 
-        <div className="w-full shadow-md min-h-full flex flex-col gap-[15px] justify-center items-center p-[20px]">
+        <div
+          className="w-full shadow-md min-h-full flex flex-col gap-[20px] justify-center items-center p-[20px]"
+          value="container"
+        >
+          {container ? "" : <p>No tasks added</p>}
           {allItems?.map(({ completed, id, task }) => (
-            <div className="w-full flex justify-between items-center" key={id}>
+            <div className="w-full flex justify-between items-end" key={id}>
               <p>
-                <label
-                  htmlFor=""
-                  className="flex justify-center items-center gap-[5px]"
-                >
+                <label className="flex justify-center items-baseline gap-[5px]">
                   <input
                     type="checkbox"
                     checked={completed}
-                    onChange={() => {
-                      setDone(!done);
-                    }}
+                    onChange={(e) => toggleStatus(id, e)}
                   />
-                  <span className={completed && "line-through"}>{task}</span>
+                  <span
+                    className={`font-medium transition-all ease-linear ${
+                      completed && "line-through"
+                    }`}
+                  >
+                    {task}
+                  </span>
                 </label>
               </p>
-              <button className="cursor-pointer bg-red-600 px-[15px] py-[5px] text-white font-semibold">
+              <button
+                className="cursor-pointer bg-red-600 px-[15px] py-[5px] text-white font-semibold"
+                onClick={() => deleteTask(id)}
+              >
                 Del
               </button>
             </div>
