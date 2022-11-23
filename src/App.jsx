d3c4ebab0f8078729bb144dc02e4d6dfc2f25d5a
Cornerstone <fortunecornerstone@gmail.com>
2022-11-23 12:59:23 +0100

    feat: Store tasks in db and display tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React, { useEffect, useState } from "react";
-import Checkbox from "./components/Checkbox";
+import { Dexie } from "dexie";
+import { useLiveQuery } from "dexie-react-hooks";
+
+const db = new Dexie("todoApp");
+db.version(1).stores({
+  todos: "++id, task, completed, date",
+});
+
+const { todos } = db;
 
 const App = () => {
+  const allItems = useLiveQuery(() => todos.toArray(), []);
+
   const [displayArrow, setDisplayArrow] = useState(false);
   const [done, setDone] = useState(false);
-  const [todo, setTodo] = useState("");
+  const [taskInput, setTaskInput] = useState("");
 
   const scrollUp = () => {
     window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
@@ -26,23 +36,24 @@ const App = () => {
     return () => window.removeEventListener("scroll", listenToScroll);
   }, []);
 
-  const handleSubmit = (e) => {
+  const addTask = async (e) => {
     e.preventDefault();
-    if (todo) {
-      alert(todo);
-    } else {
-      alert("please input a todo");
-    }
+
+    await todos.add({
+      task: taskInput,
+      completed: false,
+    });
+
+    setTaskInput("");
   };
 
   return (
     <div className="w-full min-h-screen flex justify-center items-start py-[30px]">
-      <div className="w-[90%] md:w-[50%] flex flex-col gap-[40px] items-center">
+      <div className="w-[90%] lg:w-[50%] flex flex-col gap-[40px] items-center">
         <h1 className="text-teal-700 font-bold text-2xl">Todo App</h1>
-        {/* todo form */}
         <form
           className="w-full flex flex-col gap-[6px] justify-end items-end"
-          onSubmit={handleSubmit}
+          onSubmit={addTask}
         >
           <input
             type="text"
@@ -50,8 +61,8 @@ const App = () => {
             id=""
             placeholder="What are you doing today?"
             className="w-full outline-none border-b-2 border-teal-700 pb-[8px] pl-[5px]"
-            value={todo}
-            onChange={(e) => setTodo(e.target.value)}
+            value={taskInput}
+            onChange={(e) => setTaskInput(e.target.value)}
           />
           <button className="text-white font-semibold text-base bg-teal-700 px-[20px] py-[5px] uppercase shadow-md shadow-slate-500">
             Add
@@ -59,34 +70,31 @@ const App = () => {
         </form>
 
         <div className="w-full shadow-md min-h-full flex flex-col gap-[15px] justify-center items-center p-[20px]">
-          <div className="w-full flex justify-between items-center">
-            <p>
-              <label
-                htmlFor=""
-                className="flex justify-center items-center gap-[5px]"
-              >
-                <input
-                  type="checkbox"
-                  name=""
-                  id=""
-                  onChange={() => {
-                    setDone(!done);
-                  }}
-                />
-                <span className={done && "line-through"}>
-                  Call Kayy to check on her
-                </span>
-              </label>
-            </p>
-            <button
-              className="cursor-pointer bg-red-600 px-[15px] py-[5px] text-white font-semibold"
-              onClick={() => alert("Deleted")}
-            >
-              Del
-            </button>
-          </div>
+          {allItems?.map(({ completed, id, task }) => (
+            <div className="w-full flex justify-between items-center" key={id}>
+              <p>
+                <label
+                  htmlFor=""
+                  className="flex justify-center items-center gap-[5px]"
+                >
+                  <input
+                    type="checkbox"
+                    checked={completed}
+                    onChange={() => {
+                      setDone(!done);
+                    }}
+                  />
+                  <span className={completed && "line-through"}>{task}</span>
+                </label>
+              </p>
+              <button className="cursor-pointer bg-red-600 px-[15px] py-[5px] text-white font-semibold">
+                Del
+              </button>
+            </div>
+          ))}
         </div>
       </div>
+
       {displayArrow && (
         <button className="fixed bottom-[40px] right-[10px]" onClick={scrollUp}>
           Up
